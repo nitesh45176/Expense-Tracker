0581ad9c9f9c61342d712b2cff2c3365a1dd6f60
Nitesh Mishra <mishranitesh45176@gmail.com>
2025-08-10 16:15:52 +0530

    Fix income chart data function

--- frontend/src/components/Dashboard/Last30DaysExpenses.jsx
+++ frontend/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { prepareExpenseBarChartData } from "../../utils/helper";
+
 import CustomBarChart from "../Charts/CustomBarChart";
+import { prepareExpenseBarChartData } from "../../utils/helper";
 
 
 const Last30DaysExpenses = ({ data }) => {
